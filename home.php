c063408fb28753dc8a7f5790610b8edafa81ebd2
saad-agbl <the admin>
2014-07-18 16:20:47 +0400

    first home page

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content="IE=10.000"  http-equiv="X-UA-Compatible">
+<meta charset="utf-8">     
+<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+<meta name="description" content="Alliance Bio Inc. ( Alliance Bio ) is a US-based company that specializes in supplying high quality Life Sciences products to academia and industrial research customers.
+As part of our quality commitment, we only work with reliable and renowned manufacturers for all our products; ensuring superior product ensuring superior product quality and providing all the necessary product support documentation to our customers.
+The Alliance Bio team has decades of experience in the Life Sciences, and rich scientific background that enables Alliance Bio to develop and choose the best products for its customers.">     
+<meta name="keywords" content="Alliance Bio,Biomedical Gateway, Life Sciences," />
+<title>Alliance Bio Inc | Home</title>
+
+</body>
+</html>
